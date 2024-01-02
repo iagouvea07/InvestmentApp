92abbed41c31a451a0655fe8fe9c2895d045faab
iagouvea07 <igouvea@outlook.com>
2024-01-01 21:22:37 -0300

    creating codePileline Workflow

--- scripts/backupAPI.sh
+++ scripts/backupAPI.sh
@@ -2,7 +2,7 @@
 
 function backupAPI(){
     find /backup -type f -mtime +2 -exec rm {} \;
-    tar -czvf /backup/api_$(date +'%d-%m-%Y_%H-%M-%S').tar.gz /opt/app/*
+    tar -czvf /backup/api_$(date +'%d-%m-%Y_%H-%M-%S').tar.gz /opt/app/
 }
 
 if [ -d /backup ]; then
